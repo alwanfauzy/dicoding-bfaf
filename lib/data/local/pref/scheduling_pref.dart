1c4b024e7b9c15e330ab4cb5af3573aa3770393c
alwanfauzy <alwanfauzi13@gmail.com>
2023-04-21 07:29:31 +0700

    refactor code

--- lib/data/local/pref/scheduling_pref.dart
+++ lib/data/local/pref/scheduling_pref.dart
@@ -1,15 +1,15 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SchedulingPref {
-  static const SCHEDULING_STATUS = "scheduling_status";
+  static const schedulingStatus = "scheduling_status";
 
   setScheduling(bool value) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setBool(SCHEDULING_STATUS, value);
+    prefs.setBool(schedulingStatus, value);
   }
 
   Future<bool> getScheduling() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    return prefs.getBool(SCHEDULING_STATUS) ?? false;
+    return prefs.getBool(schedulingStatus) ?? false;
   }
 }
